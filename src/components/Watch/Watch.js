aa5d48209a8a267cd32aa5675e010d5eda0b20d4
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-21 16:12:20 +0600

    some components

---
+++ src/components/Watch/Watch.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Watch = () => {
+    const [steps, setSteps] = useState(0);
+
+    const increaseSteps = () => {
+        const newStepsCount = steps + 1;
+        setSteps(newStepsCount); 
+        // console.log(steps)
+    }
+  
+    return (
+        <div>
+            <h3>I need a smart watch</h3>
+            <h5>My current steps: {steps}</h5>
+            <button onClick={increaseSteps}>Count steps</button>
+        </div>
+    );
+};
+
+export default Watch;
